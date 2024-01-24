3ed67b31320f5019c6fe6963cd63eb1f54d80341
JulienDast <julien.dast44@gmail.com>
2024-01-24 18:29:59 +0100

    client side

---
+++ padel_frontend/src/pages/Profil.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Log from '../components/Log';
+
+const Profil = () => {
+  return (
+    <div>
+      <div className="profil-page">
+        <div className="log-container">
+          <Log/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profil;
